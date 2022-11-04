16b4c94a4ef93335df7f41dbf96f7372174205a3
William Faircloth <willfaircloth@icloud.com>
2022-11-04 16:45:50 +0000

    use perm url for tests

--- test/canvas.test.ts
+++ test/canvas.test.ts
@@ -3,8 +3,6 @@ import gendap from "../src/lib/gendap";
 test("generated image is type buffer", async () => {
 	type TestUser = { displayAvatarURL: () => string };
 	const testuser = {
-		displayAvatarURL: () =>
-			"https://media.discordapp.net/attachments/1028766392989794444/1029202069812424764/dapper.png?width=406&height=406",
-	};
+		displayAvatarURL: () => "https://github.com/willuhm-js/dapper/blob/master/src/images/test.jpg?raw=true" };
 	expect(await gendap(testuser, testuser)).toBeInstanceOf(Buffer);
 }, 20000);
