af2385e0709da7462c6b66eb0f04c6082a2913b3
William Faircloth <willfaircloth@icloud.com>
2022-11-04 19:58:44 +0000

    meta changes, test changes

--- test/canvas.test.ts
+++
@@ -1,8 +0,0 @@
-import gendap from "../src/lib/gendap";
-
-test("generated image is type buffer", async () => {
-	type TestUser = { displayAvatarURL: () => string };
-	const testuser = {
-		displayAvatarURL: () => "https://github.com/willuhm-js/dapper/blob/master/src/images/test.jpg?raw=true" };
-	expect(await gendap(testuser, testuser)).toBeInstanceOf(Buffer);
-}, 20000);
