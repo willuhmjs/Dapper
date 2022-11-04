c43d5839f06607270d1b0354ca0dcfcdb347f642
William Faircloth <willuhmjs@gmail.com>
2022-11-04 14:09:32 -0400

    provide comments

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,16 @@
 import { config } from "dotenv";
 config();
 
+// Discord bot token
 export const token = process.env.TOKEN;
+
+// Discord bot client id
 export const clientId = process.env.CLIENTID;
+
+// MongoDB connection string URI
+// https://www.mongodb.com/docs/manual/reference/connection-string/
 export const mongo = process.env.MONGO;
+
+// The cooldown between daps before dapscore is awarded again, in milliseconds
+// default is 1 minute
 export const cooldown = 60 * 1000;
