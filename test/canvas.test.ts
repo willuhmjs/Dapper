e54b6e6843c0219927ee12bd4e0e0b8e14be4feb
William Faircloth <willfaircloth@icloud.com>
2022-11-04 03:45:50 +0000

    simplify test

--- test/canvas.test.ts
+++ test/canvas.test.ts
@@ -3,8 +3,6 @@ import { writeFileSync } from "node:fs";
 
 test("generated image is type buffer", async () => {
 	type TestUser = { displayAvatarURL: () => string }
-	const TestUser = { displayAvatarURL: () => "https://media.discordapp.net/attachments/1028766392989794444/1029202069812424764/dapper.png?width=406&height=406" }
-	const image = await gendap(TestUser, TestUser)
-	writeFileSync("./file.png", image)
-	expect(image).toBeInstanceOf(Buffer)
+	const testuser = { displayAvatarURL: () => "https://media.discordapp.net/attachments/1028766392989794444/1029202069812424764/dapper.png?width=406&height=406" }
+	expect(await gendap(testuser, testuser)).toBeInstanceOf(Buffer)
 }, 20000);
