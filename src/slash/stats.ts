0b4ae2ebb402dfa0f7733a6490e478b733981ca4
Tristan F <LeoDog896@hotmail.com>
2022-11-08 02:26:52 +0000

    Prettify

--- src/slash/stats.ts
+++ src/slash/stats.ts
@@ -44,16 +44,13 @@ export default <CommandLike>{
 			member
 		);
 
-		let UserGuildData =
-			(await GuildDapSchema.findOne({
-				userId: member.id,
-				guildId: interaction.guild.id,
-			}));
+		let UserGuildData = await GuildDapSchema.findOne({
+			userId: member.id,
+			guildId: interaction.guild.id,
+		});
 
 		if (!UserGuildData) {
-			return embedError(
-				`${member.user.username} has not been dapped yet.`
-			);
+			return embedError(`${member.user.username} has not been dapped yet.`);
 		}
 
 		const replyEmbed = new EmbedBuilder()
