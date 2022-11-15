4dcc7764c32aea5e9f4a64f8b0bde0dda9874917
William Faircloth <willuhmjs@gmail.com>
2022-11-15 01:57:47 +0000

    begin stuff

---
+++ src/slash/edit.ts
@@ -0,0 +1,100 @@
+import { subtle } from "crypto";
+import {
+	SlashCommandBuilder,
+	EmbedBuilder,
+	PermissionsBitField,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	ModalBuilder,
+	TextInputBuilder,
+	TextInputStyle,
+} from "discord.js";
+import { GuildDapSchema } from "../models";
+import type { CommandLike } from "./command";
+
+export default <CommandLike>{
+	data: new SlashCommandBuilder()
+		.setName("edit")
+		.setDescription("An administrator command to edit the profile of a user.")
+		.addSubcommand(subcommand => 
+			subcommand
+			.setName("edit")
+			.setDescription("Edit a user's profile.")
+			.addUserOption(option =>
+				option.setName("user").setDescription("The user to edit.")
+			)
+			
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+			.setName("delete")
+			.setDescription("Delete a user's profile.")
+			.addUserOption(option =>
+				option.setName("user").setDescription("The user to delete.")
+			)
+		)
+		.setDefaultMemberPermissions(PermissionsBitField.Flags.ManageGuild),
+	async execute(client, interaction) {
+		const user = interaction.options.getUser("user") || interaction.user;
+		const UserGuildData =
+			(await GuildDapSchema.findOne({
+				userId: user.id,
+				guildId: interaction.guild.id,
+			})) ||
+			(await new GuildDapSchema({
+				userId: user.id,
+				guildId: interaction.guild.id,
+			}).save());
+
+
+
+		if (interaction.options.getSubcommand() === "edit") {
+			const modal = new ModalBuilder()
+				.setTitle(`Editing ${user.username}'s profile`)
+				.setCustomId("edit_profile_modal");
+			const DapScoreInput = new TextInputBuilder()
+				.setCustomId("dap_score_input")
+				.setPlaceholder("DapScore")
+				.setValue(""+UserGuildData.userDap || "0")
+				.setMinLength(1)
+				.setStyle(TextInputStyle.Short);
+
+			const DapsGivenInput = new TextInputBuilder()
+				.setCustomId("daps_given_input")
+				.setPlaceholder("Daps Given")
+				.setValue(""+UserGuildData.dapsGiven || "0")
+				.setMinLength(1)
+				.setStyle(TextInputStyle.Short);
+
+			const DapsRecievedInput = new TextInputBuilder()
+				.setCustomId("daps_recieved_input")
+				.setPlaceholder("Daps Recieved")
+				.setValue(""+UserGuildData.dapsRecieved || "0")
+				.setMinLength(1)
+				.setStyle(TextInputStyle.Short);
+
+			const firstActionRow = new ActionRowBuilder().addComponents(
+				DapScoreInput
+			);
+
+			const secondActionRow = new ActionRowBuilder().addComponents(
+				DapsGivenInput
+			);
+
+			const thirdActionRow = new ActionRowBuilder().addComponents(
+				DapsRecievedInput
+			);
+
+			// @ts-ignore:code
+			modal.addComponents(firstActionRow, secondActionRow, thirdActionRow);
+			await interaction.showModal(modal);
+		} else if (interaction.options.getSubcommand() === "delete") {
+			/*await UserGuildData.deleteOne();
+			await interaction.reply({
+				content: `Deleted ${user.username}'s profile.`,
+				ephemeral: true,
+			});*/
+		}
+	},
+};
--- src/slash/index.ts
+++ src/slash/index.ts
@@ -4,5 +4,6 @@ import about from "./about";
 import dap from "./dap";
 import leaderboard from "./leaderboard";
 import stats from "./stats";
+import edit from "./edit";
 
-export const commands: CommandLike[] = [about, dap, leaderboard, stats];
+export const commands: CommandLike[] = [about, dap, leaderboard, stats, edit];
